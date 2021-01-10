2c926b9effebb9907afffc57f0da67bb078cdf13
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-10 03:01:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    p src[row_index][element_index]
+    if src[row_index][element_index] % 2 == 0
     element_index += 1
   end
   row_index += 1
